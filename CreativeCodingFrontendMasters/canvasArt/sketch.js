2ade2380e718deb86d61e1bd9d16f81bb8f40ecc
Max Manthey <mantheymax23@gmail.com>
2021-09-23 16:50:16 +0200

    finished canvas stuff

--- CreativeCodingFrontendMasters/canvasArt/sketch.js
+++ CreativeCodingFrontendMasters/canvasArt/sketch.js
@@ -4,16 +4,12 @@ const random = require('canvas-sketch-util/random')
 const palettes = require('nice-color-palettes')
 
 const settings = {
-  dimensions: [2048, 2048]//[2048, 2048]//'A4'
-  // dimensions: [2560, 1600]
+  // dimensions: [2048, 2048]//[2048, 2048]//'A4'
+  dimensions: [2560, 1600]
   // orientation: 'landscape',
   // units: 'cm',
   // pixelsPerInch: 300
 };
-
-//TODO add random symbols
-//TODO adapt for MacBook Screensize
-//TODO make it easy to change between square and MacBook size
 
 random.setSeed(random.getRandomSeed())
 console.log(random.getSeed())
@@ -22,22 +18,24 @@ const sketch = () => {
   const colorCount = random.rangeFloor(2, 6)
   const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount)
   console.log(palette)
+  const possibleSymbols = ["-", "<", ">", "=", "!", "'", "/", ".", ":", ";"]
 
   const createGrid = () => {
     const points = []
-    const count = 70
-    // for (let x = 0.5; x < count-1; ++x) {
-      // for (let y = 0; y < count-9; ++y) {
-    for (let x = 0; x < count; ++x) {
-      for (let y = 0; y < count; ++y) {
+    const count = 60
+    for (let x = 1; x < count-3; ++x) {
+      for (let y = 2; y < count-25; ++y) {
+    // for (let x = 0; x < count; ++x) {
+      // for (let y = 0; y < count; ++y) {
         const u = count <= 1 ? 0.5 : x / (count - 1)
         const v = count <= 1 ? 0.5 : y / (count - 1)
-        const radius = Math.abs(random.noise2D(u, v)) * 0.2
+        const radius = Math.abs(random.noise2D(u, v)) * 0.15
         points.push({
           color: random.pick(palette),
           radius, //: Math.abs(0.01 + random.gaussian() * 0.03),
           position: [u, v],
-          rotation: random.noise2D(u, v)
+          rotation: random.noise2D(u, v),
+          symbol: "-"//random.pick(possibleSymbols)
         })
       }
     }
@@ -54,7 +52,7 @@ const sketch = () => {
     context.fillRect(0, 0, width, height)
 
     points.forEach(data => {
-      const {radius, position, color, rotation} = data
+      const {radius, position, color, rotation, symbol} = data
       const [u, v]  = position
       const x = lerp(margin, width - margin, u)
       const y = lerp(margin, width - margin, v)
@@ -73,7 +71,7 @@ const sketch = () => {
       context.font = size+'px "Helvetica"'
       context.translate(x, y)
       context.rotate(rotation)
-      context.fillText('-', 0, 0)
+      context.fillText(symbol, 0, 0)
       context.restore()
     })
   };
