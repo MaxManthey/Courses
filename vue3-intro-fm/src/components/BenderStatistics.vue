7c57ce1b75370dc4d465979cbc92f87df9e93829
Max Manthey <mantheymax23@gmail.com>
2024-06-28 17:48:16 +0200

    vue3 script-setup-exercise

--- vue3-intro-fm/src/components/BenderStatistics.vue
+++ vue3-intro-fm/src/components/BenderStatistics.vue
@@ -4,45 +4,37 @@
   </ul>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import type { Character } from '@/models/Character.type'
-import { computed, defineComponent, type PropType } from 'vue'
+import { computed, type PropType } from 'vue'
 
-export default defineComponent({
-  name: 'BenderStatistics',
-  props: {
-    characterList: {
-      required: true,
-      type: Array as PropType<Character[]>
-    }
-  },
-  setup(props) {
-    const bendingSummary = computed(() => {
-      const statistics = {
-        Fire: 0,
-        Water: 0,
-        Earth: 0,
-        Air: 0,
-        None: 0
-      }
-
-      props.characterList.forEach((character) => {
-        character.element.forEach((element) => {
-          statistics[element]++
-        })
-      })
-
-      props.characterList.forEach(({ element }) => {
-        if (element.length === 0) statistics['None']++
-      })
-
-      return statistics
-    })
-
-    return {
-      bendingSummary
-    }
+const props = defineProps({
+  characterList: {
+    required: true,
+    type: Array as PropType<Character[]>
   }
+})
+
+const bendingSummary = computed(() => {
+  const statistics = {
+    Fire: 0,
+    Water: 0,
+    Earth: 0,
+    Air: 0,
+    None: 0
+  }
+
+  props.characterList.forEach((character) => {
+    character.element.forEach((element) => {
+      statistics[element]++
+    })
+  })
+
+  props.characterList.forEach(({ element }) => {
+    if (element.length === 0) statistics['None']++
+  })
+
+  return statistics
 })
 </script>
 
